c7a8c9a6eb03a9761fa2d9b4c23365d554117a28
oh1701 <rbtjd2290@naver.com>
2024-07-18 18:24:04 +0900

    버전코드 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 4
-        versionName = "1.2"
+        versionName = "1.3"
 //////123123//////
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
