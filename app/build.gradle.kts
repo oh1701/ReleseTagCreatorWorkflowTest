3102a6a0eea32920b3373eca604c8f65d9837a7b
oh1701 <rbtjd2290@naver.com>
2024-07-18 18:09:37 +0900

    5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         targetSdk = 34
         versionCode = 3
         versionName = "1.1"
-//////
+//////123123
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
