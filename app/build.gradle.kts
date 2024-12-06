d76e2ba85a96233e8c89026d8d4ac751a0492fa4
oh1701 <rbtjd2290@naver.com>
2024-12-07 03:22:32 +0900

    테스트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 3
-        versionName = "1.94"
+        versionName = "1.95"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
